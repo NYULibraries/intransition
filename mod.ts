1b002822b3cf4ba1ee39f5dfae8afff4f86f95c1
Alberto Ortiz Flores <aortiz@nyu.edu>
2023-07-10 21:59:50 -0400

    first commit

---
+++ mod.ts
@@ -0,0 +1,31 @@
+import { slugify } from "https://deno.land/x/slugify/mod.ts";
+
+const { writeTextFile } = Deno
+
+const res = await fetch('http://localhost/intransition/api/v1/spokes')
+
+const body = await res.text()
+
+const request = JSON.parse(body)
+
+// Bad
+
+// http://localhost/intransition/api/v1/node/192
+// http://localhost/intransition/api/v1/node/176
+// http://localhost/intransition/api/v1/node/82
+// http://localhost/intransition/api/v1/node/186
+// http://localhost/intransition/api/v1/node/180
+// http://localhost/intransition/api/v1/node/188
+// http://localhost/intransition/api/v1/node/56
+// http://localhost/intransition/api/v1/node/72
+// http://localhost/intransition/api/v1/node/182
+// http://localhost/intransition/api/v1/node/978
+
+request.response.docs.forEach(async (document) => {
+  const res = await fetch(document.path)
+  if (res.status == 200) {
+    const body = await res.json()
+    const label = slugify(document.title, { separator: '-', lower: true, remove: /[*+~.()'"!:@]/g })
+    await writeTextFile(`./entities/${label}.json`, JSON.stringify(body, null, 2))
+  }
+})
