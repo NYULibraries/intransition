ac6600b7d68eea9a4169c212c502ccaa69b4f789
Alberto Ortiz Flores <aortiz@nyu.edu>
2023-07-17 12:51:41 -0400

    new data

--- mod.ts
+++ mod.ts
@@ -8,19 +8,6 @@ const body = await res.text()
 
 const request = JSON.parse(body)
 
-// Bad
-
-// http://localhost/intransition/api/v1/node/192
-// http://localhost/intransition/api/v1/node/176
-// http://localhost/intransition/api/v1/node/82
-// http://localhost/intransition/api/v1/node/186
-// http://localhost/intransition/api/v1/node/180
-// http://localhost/intransition/api/v1/node/188
-// http://localhost/intransition/api/v1/node/56
-// http://localhost/intransition/api/v1/node/72
-// http://localhost/intransition/api/v1/node/182
-// http://localhost/intransition/api/v1/node/978
-
 request.response.docs.forEach(async (document) => {
   const res = await fetch(document.path)
   if (res.status == 200) {
